078f5a89831d6d1ca2a60e62a9facf75fad8a5f7
Tomasz <n1kula@wp.pl>
2024-01-31 14:02:33 +0100

    Unit Tests Initiated

---
+++ UnitTests/ClientTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using C_SHARP_CODING;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ClientTest
+    {
+        [TestMethod]
+        public void FirstLastNameTest()
+        {
+            Client client = new Client();
+            client.FirstName = "Tom";
+            client.LastName = "Kowal";
+            string expectedResult = "Tom Kowal";
+
+            string actualResult = client.FirstLastName;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
