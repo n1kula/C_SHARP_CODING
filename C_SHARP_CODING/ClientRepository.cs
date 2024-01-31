4423236ebf02b7bdcffae462c53c855581f62682
Tomasz <n1kula@wp.pl>
2024-01-31 18:35:54 +0100

    ClientRepository added

---
+++ C_SHARP_CODING/ClientRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_SHARP_CODING
+{
+    public class ClientRepository
+    {
+        /// <summary>
+        /// Save current client
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            //save client
+            return true;
+        }
+
+        /// <summary>
+        /// Get data of specific client
+        /// </summary>
+        /// <param name="clientId"></param>
+        /// <returns></returns>
+        public Client getData(int clientId)
+        {
+            Client client = new Client(clientId);
+            //temp data
+            if (clientId == 1)
+            {
+                client.Email = "email@email.com";
+                client.FirstName = "tom";
+                client.LastName = "kowalski";
+
+            }
+            return client;
+        }
+
+        /// <summary>
+        /// Get all clients data
+        /// </summary>
+        /// <returns></returns>
+        public List<Client> getData()
+        {
+            //get all clients
+            return new List<Client>();
+        }
+    }
+}
---
+++ UnitTests/ClientRepositoryTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using C_SHARP_CODING;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ClientRepositoryTest
+    {
+        [TestMethod]
+        public void GetClient()
+        {
+            //Arrange
+            var clientRepository = new ClientRepository();
+            var expectedResult = new Client(1)
+            {
+                Email = "email@email.com",
+                FirstName = "tom",
+                LastName = "kowalski"
+            };
+
+            //Act
+            var currentResult = clientRepository.getData(1);
+
+            //Assert
+            Assert.AreEqual(expectedResult.ClientId, currentResult.ClientId);
+            Assert.AreEqual(expectedResult.Email, currentResult.Email);
+            Assert.AreEqual(expectedResult.FirstName, currentResult.FirstName);
+            Assert.AreEqual(expectedResult.LastName, currentResult.LastName);
+        }
+    }
+}
