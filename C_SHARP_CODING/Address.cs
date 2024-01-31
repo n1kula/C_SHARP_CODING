8e08641acf2c1febf4066a765b604416b16fbdfd
Tomasz <n1kula@wp.pl>
2024-01-31 18:36:08 +0100

    Address class added

---
+++ C_SHARP_CODING/Address.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_SHARP_CODING
+{
+    public class Address
+    {
+        public Address()
+        {
+            
+        }
+
+        public Address(int addressId)
+        {
+            AddressId = addressId;
+        }
+
+        public int AddressId { get; private set; }
+        public int AddressType { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+    }
+}
