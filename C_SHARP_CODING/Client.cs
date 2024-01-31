cb610dd4dac3842aea9f9beea8c8265d96154c3f
Tomasz <n1kula@wp.pl>
2024-01-31 13:58:04 +0100

    Initial commit

---
+++ C_SHARP_CODING/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_SHARP_CODING
+{
+    public class Client
+    {
+        private string _lastName;
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+        public string FirstName { get; set; }
+
+        public string Email { get; set; }
+
+        public int ClientId { get; private set; }
+
+        public string FirstLastName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+    }
+}
